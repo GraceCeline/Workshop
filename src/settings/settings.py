e31fc428943ad7a78adef0a756354bcdfcf831d5
GraceCeline <graceceline.florensia@gmail.com>
2025-01-30 13:10:56 +0000

    changed requirements

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -123,12 +123,25 @@ WSGI_APPLICATION = 'src.settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv("DOCKERIZED") == "1":
+    print('Using MYSQL')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('MYSQL_DATABASE', 'django_db'),
+            'USER': os.getenv('MYSQL_USER', 'grace'),
+            'PASSWORD': os.getenv('MYSQL_PASSWORD', 'celine123'),
+            'HOST': os.getenv('MYSQL_HOST', 'mysql'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
